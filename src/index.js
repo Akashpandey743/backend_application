d0561a29b49c562bb48844c5683110eab021f30b
Akash Pandey <Akash pandey>
2024-04-27 14:21:09 +0530

    custom api response and error hanling in utils

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port ${process.env.PORT} `)
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed !!", err)
+})
